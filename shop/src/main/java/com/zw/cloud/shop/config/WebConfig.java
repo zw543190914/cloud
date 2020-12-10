cecf1f0b72688d424cb1f98b3ee151c12063d049
zhengwei <543190914@qq.com>
2020-12-10 23:30:39 +0800

    update shop

--- shop/src/main/java/com/zw/cloud/shop/config/WebConfig.java
+++ shop/src/main/java/com/zw/cloud/shop/config/WebConfig.java
@@ -7,11 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig  extends WebMvcConfigurerAdapter{
+public class WebConfig  implements WebMvcConfigurer {
 	
 	@Autowired
 	UserArgumentResolver userArgumentResolver;
@@ -28,5 +27,5 @@ public class WebConfig  extends WebMvcConfigurerAdapter{
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(accessInterceptor);
 	}
-	
+
 }
