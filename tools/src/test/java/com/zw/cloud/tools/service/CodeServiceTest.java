5067db4439c3dd7bcddf0b983f94fd73e3d22ee5
镜中水月 <8344832+moon-in-the-mirror@user.noreply.gitee.com>
2021-09-21 01:00:34 +0800

    junit 测试静态方法

--- tools/src/test/java/com/zw/cloud/tools/service/CodeServiceTest.java
+++ tools/src/test/java/com/zw/cloud/tools/service/CodeServiceTest.java
@@ -5,20 +5,28 @@ import com.google.common.collect.Lists;
 import com.zw.cloud.tools.dao.CodeDao;
 import com.zw.cloud.tools.entity.Code;
 import com.zw.cloud.tools.service.impl.CodeServiceImpl;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import com.zw.cloud.tools.utils.DateTimeUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+
 import java.util.Date;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 
-@ExtendWith(MockitoExtension.class)
-class CodeServiceTest {
+//@ExtendWith(MockitoExtension.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({DateTimeUtils.class})
+public class CodeServiceTest {
 
     //将生成MockDao，并注入到@InjectMocks指定的类中
     @Mock
@@ -27,19 +35,30 @@ class CodeServiceTest {
     @InjectMocks
     CodeServiceImpl codeService;
 
+    @Before
+    public void before() {
+        PowerMockito.mockStatic(DateTimeUtils.class);
+    }
+
     @Test
-    void queryAllByLimit() {
+    public void queryAllByLimit() {
         Mockito.when(codeDao.queryAllByLimit(anyInt(), anyInt())).thenReturn(Lists.newArrayList());
         List<Code> codes = codeService.queryAllByLimit(1, 20);
         System.out.println(JSON.toJSONString(codes));
     }
 
     @Test
-    void insert() {
+    public void insert() {
         Code code = new Code();
         code.setGmtCreate(new Date());
         code.setStatus((byte)2);
         Mockito.doNothing().when(codeDao).insert(any());
         codeService.insert(code);
+    }
+
+    @Test
+    public void plusMonths() {
+        PowerMockito.when(DateTimeUtils.plusMonths(anyInt())).thenReturn(new Date());
+        System.out.println(DateTimeUtils.plusMonths(1));
     }
 }
