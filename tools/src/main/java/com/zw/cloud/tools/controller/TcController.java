a8783882335aac22dd29869d6c146834406b92bb
镜中水月 <8344832+moon-in-the-mirror@user.noreply.gitee.com>
2021-10-11 23:35:59 +0800

    add test

--- tools/src/main/java/com/zw/cloud/tools/controller/TcController.java
+++ tools/src/main/java/com/zw/cloud/tools/controller/TcController.java
@@ -1,17 +1,19 @@
 package com.zw.cloud.tools.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageHelper;
+import com.google.common.collect.Lists;
 import com.zw.cloud.common.utils.HttpClientUtils;
 import com.zw.cloud.tools.dao.TcMapper;
 import com.zw.cloud.tools.entity.Tc;
+import com.zw.cloud.tools.entity.TcExample;
 import com.zw.cloud.tools.modle.vo.TcResultVO;
 import com.zw.cloud.tools.service.TcService;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -46,7 +48,7 @@ public class TcController {
     @GetMapping("/add")
     //http://localhost:9040/tc/add
     public void add(){
-        for (int i = 1; i <= 73; i++) {
+        for (int i = 1; i <= 10; i++) {
             String url = "https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=85&provinceId=0&pageSize=30&isVerify=1&pageNo="+ i;
             String result = HttpClientUtils.doGet(url, null, null);
             TcResultVO tcResultVO = JSON.parseObject(result, TcResultVO.class);
@@ -77,4 +79,52 @@ public class TcController {
 
     }
 
+    @GetMapping("/query")
+    //http://localhost:9040/tc/query
+    public void query() throws Exception{
+        TcExample example = new TcExample();
+        example.setOrderByClause("id desc");
+        PageHelper.startPage(1,3);
+        List<Tc> tcList = tcMapper.selectByExample(example);
+        List<Integer> code = new ArrayList<>(20);
+        tcList.forEach(tc -> {
+            code.add(tc.getOne());
+            code.add(tc.getTwo());
+            code.add(tc.getThree());
+            code.add(tc.getFour());
+            code.add(tc.getFive());
+        });
+        Collections.sort(code);
+        List<Integer> codeList = code.stream().distinct().sorted(Comparator.comparingInt(s -> s)).collect(Collectors.toList());
+        System.out.println(JSON.toJSONString(codeList));
+        codeList.add(0);
+        System.out.println("==================");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(query(codeList));
+            Thread.sleep(1000);
+        }
+    }
+
+    private List<Integer> query(List<Integer> codeList) {
+        Set<Integer> set = new HashSet<>();
+        while (true) {
+            Random random = new Random();
+            int i = random.nextInt(36);
+            if (!codeList.contains(i)) {
+                set.add(i);
+            }
+            if (set.size() > 4) {
+                ArrayList<Integer> result = Lists.newArrayList(set);
+                Collections.sort(result);
+                return result;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        while (true) {
+            System.out.println(random.nextInt(36));
+        }
+    }
 }
