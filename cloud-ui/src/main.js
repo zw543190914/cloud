80379caffc6ae9ea97a54663b480e23cda6d87e8
zhengwei <zhengwei5064@zj.tech>
2021-12-21 21:28:59 +0800

    add cloud-ui

---
+++ cloud-ui/src/main.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// 按需引入
+import {  Aside,Button,
+  Container,
+  DatePicker,
+  Dropdown, DropdownMenu, DropdownItem,
+  Header,
+
+  Menu,Submenu,MenuItem,MenuItemGroup,
+  Main,
+  RadioGroup,
+  Row,
+  Table, TableColumn,
+  }
+  from 'element-ui';
+
+Vue.config.productionTip = false
+Vue.use(Aside);
+Vue.use(Button);
+Vue.use(Container);
+
+
+Vue.use(DatePicker);
+
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Header);
+
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(MenuItemGroup);
+Vue.use(Main);
+
+Vue.use(RadioGroup);
+Vue.use(Row);
+Vue.use(Table);
+Vue.use(TableColumn);
+
+
+
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
