ec3fa6a8f40153573a3ad55adc373ee74eb5bd68
zhengwei <zhengwei5064@zj.tech>
2022-07-29 17:18:28 +0800

    sentinel

--- sentinel/src/main/java/com/zw/cloud/sentinel/SentinelApplication.java
+++ sentinel/src/main/java/com/zw/cloud/sentinel/SentinelApplication.java
@@ -1,6 +1,5 @@
 package com.zw.cloud.sentinel;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +8,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
-@MapperScan(basePackages = "com.zw.cloud.db.dao")
 public class SentinelApplication {
 
     public static void main(String[] args) {
