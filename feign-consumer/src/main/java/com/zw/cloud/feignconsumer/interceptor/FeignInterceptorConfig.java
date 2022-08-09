03d3be473afa400f205ca4af7e99620537cde9c1
镜中水月 <8344832+moon-in-the-mirror@user.noreply.gitee.com>
2022-08-10 00:14:44 +0800

    Feign拦截器

--- feign-consumer/src/main/java/com/zw/cloud/feignconsumer/interceptor/FeignInterceptorConfig.java
+++ feign-consumer/src/main/java/com/zw/cloud/feignconsumer/interceptor/FeignInterceptorConfig.java
@@ -2,23 +2,34 @@ package com.zw.cloud.feignconsumer.interceptor;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @Configuration
+@Slf4j
 public class FeignInterceptorConfig implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
-        ServletRequestAttributes attributes = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes());
-        if (attributes != null) {
-            HttpServletRequest request = attributes.getRequest();
-            String access_token = request.getHeader("ACCESS_TOKEN");
-            System.out.println(access_token);
-            requestTemplate.header("ACCESS_TOKEN", access_token);
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes != null) {
+            HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+            //设置header属性：
+            requestTemplate.header("plat_source", "consumer");
+            Enumeration<String> headerNames = request.getHeaderNames();
+            if (headerNames != null) {
+                while (headerNames.hasMoreElements()) {
+                    String name = headerNames.nextElement();
+                    String value = request.getHeader(name);
+                    requestTemplate.header(name, value);
+                }
+            }
         }
     }
 
