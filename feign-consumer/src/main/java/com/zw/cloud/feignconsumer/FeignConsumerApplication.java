83de235be17bac28a63d0b318d563a161dbd5b35
镜中水月 <8344832+moon-in-the-mirror@user.noreply.gitee.com>
2022-08-18 00:38:50 +0800

    openFeign api 格式调整 消费者服务调用 openFeign okhttp

--- feign-consumer/src/main/java/com/zw/cloud/feignconsumer/FeignConsumerApplication.java
+++ feign-consumer/src/main/java/com/zw/cloud/feignconsumer/FeignConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.zw.cloud;
+package com.zw.cloud.feignconsumer;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.zw.cloud.feignproviderapi.client")
 @EnableDiscoveryClient
 @MapperScan(basePackages = "com.zw.cloud.feignconsumer.test.dao,com.zw.cloud.feignconsumer.test2.dao")
 public class FeignConsumerApplication {
