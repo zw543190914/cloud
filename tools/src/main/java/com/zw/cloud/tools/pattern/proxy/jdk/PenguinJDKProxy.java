f02da89f89c240a1a6f19cb0d0117f0063454595
zhengwei <zhengwei5064@zj.tech>
2023-01-10 15:17:53 +0800

    feat(test): #001 动态代理

---
+++ tools/src/main/java/com/zw/cloud/tools/pattern/proxy/jdk/PenguinJDKProxy.java
@@ -0,0 +1,39 @@
+package com.zw.cloud.tools.pattern.proxy.jdk;
+
+import com.alibaba.fastjson.JSON;
+import com.zw.cloud.tools.pattern.proxy.PenguinService;
+import com.zw.cloud.tools.pattern.proxy.PenguinServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.InvocationHandler;
+import org.springframework.cglib.proxy.Proxy;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class PenguinJDKProxy implements InvocationHandler {
+
+    /**
+     * 原始对象,被代理对象
+     */
+    private Object originalObj;
+
+    public PenguinJDKProxy(Object originalObj) {
+        this.originalObj = originalObj;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        log.info("[PenguinJDKProxy][invoke] before");
+        Object result = method.invoke(originalObj, args);
+        log.info("[PenguinJDKProxy][invoke] after,result is {}", JSON.toJSONString(result));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        PenguinService penguinService = new PenguinServiceImpl();
+        PenguinJDKProxy jdkProxy = new PenguinJDKProxy(penguinService);
+        ClassLoader classLoader = penguinService.getClass().getClassLoader();
+        PenguinService proxyPenguinService = (PenguinService)Proxy.newProxyInstance(classLoader, new Class[]{PenguinService.class}, jdkProxy);
+        proxyPenguinService.beat();
+    }
+}
