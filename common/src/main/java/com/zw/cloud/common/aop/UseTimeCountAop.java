85716e5d0bd9eab4e82381053bf878ac8c03a552
zhengwei <zhengwei5064@zj.tech>
2023-04-07 10:58:26 +0800

    feat(test): #001 aop 顺序测试

--- common/src/main/java/com/zw/cloud/common/aop/UseTimeCountAop.java
+++ common/src/main/java/com/zw/cloud/common/aop/UseTimeCountAop.java
@@ -1,11 +1,12 @@
 package com.zw.cloud.common.aop;
 
+import com.alibaba.fastjson.JSON;
 import com.zw.cloud.common.annotation.UseTimeCount;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -13,11 +14,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class UseTimeCountAop {
 
+    /**
+     * around before
+     * before
+     *
+     * afterReturning/afterThrowing
+     * after
+     * around after
+     */
     @Around("@annotation(useTimeCount)")
     public Object countUseTime(ProceedingJoinPoint joinPoint, UseTimeCount useTimeCount) throws Throwable{
         Signature joinPointSignature = joinPoint.getSignature();
         String className = joinPointSignature.getDeclaringTypeName();
         String methodName = joinPointSignature.getName();
+        log.info("[UseTimeCountAop][countUseTime]class is {},methodName is {}", className,methodName);
         long start = System.currentTimeMillis();
         try {
             Object result = joinPoint.proceed();
@@ -29,5 +39,37 @@ public class UseTimeCountAop {
                     className,methodName,System.currentTimeMillis() - start,throwable);
             throw throwable;
         }
+    }
+
+    @Before("@annotation(useTimeCount)")
+    public void before(JoinPoint joinPoint, UseTimeCount useTimeCount) {
+        Object[] args = joinPoint.getArgs();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        log.info("[UseTimeCountAop][before]class is {},methodName is {},args is {}",className,methodName, JSON.toJSONString(args));
+    }
+
+    @After("@annotation(useTimeCount)")
+    public void after(JoinPoint joinPoint, UseTimeCount useTimeCount) {
+        Object[] args = joinPoint.getArgs();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        log.info("[UseTimeCountAop][after]class is {},methodName is {},args is {}",className,methodName, JSON.toJSONString(args));
+    }
+
+    @AfterReturning("@annotation(useTimeCount)")
+    public void afterReturning(JoinPoint joinPoint, UseTimeCount useTimeCount) {
+        Object[] args = joinPoint.getArgs();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        log.info("[UseTimeCountAop][afterReturning]class is {},methodName is {},args is {}",className,methodName, JSON.toJSONString(args));
+    }
+
+    @AfterThrowing(value = "@annotation(useTimeCount)",throwing = "ex")
+    public void afterThrowing(JoinPoint joinPoint, UseTimeCount useTimeCount,Exception ex) {
+        Object[] args = joinPoint.getArgs();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        log.error("[UseTimeCountAop][afterThrowing]class is {},methodName is {},args is {},ex is ",className,methodName, JSON.toJSONString(args),ex);
     }
 }
