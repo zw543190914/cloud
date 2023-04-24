298c62a9a2338eede4132a8cf4a8f3f330582c41
zhengwei <zhengwei5064@zj.tech>
2023-04-24 16:16:41 +0800

    feat(test): #001 test

--- mybatis-plus/src/main/java/com/zw/cloud/mybatis/plus/test/Test.java
+++
@@ -1,7 +0,0 @@
-package com.zw.cloud.mybatis.plus.test;
-
-public class Test {
-    public static void main(String[] args) {
-
-    }
-}
--- tools/src/main/java/com/zw/cloud/tools/test/BeanCopyTest.java
+++ tools/src/main/java/com/zw/cloud/tools/test/BeanCopyTest.java
@@ -3,6 +3,8 @@ package com.zw.cloud.tools.test;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
 import com.alibaba.fastjson.JSON;
+import com.zw.cloud.common.utils.bean.test.entity.Student;
+import com.zw.cloud.common.utils.bean.test.entity.StudentVO;
 import com.zw.cloud.tools.entity.User;
 import com.zw.cloud.tools.entity.UserDTO;
 import com.zw.cloud.tools.entity.poem.Poem;
@@ -11,36 +13,14 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.BeanUtils;
 
+import java.time.LocalDateTime;
+
 public class BeanCopyTest {
 
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    public static class Person {
-        private String name;
-    }
-
-
-
     public static void main(String[] args){
-        User source = new User();
-        source.setAge((byte)12);
-        User target = new User();
-        target.setId(1L);
-        BeanUtil.copyProperties(source,target);
-        System.out.println(JSON.toJSONString(target));
-
-        User source1 = new User();
-        source1.setAge((byte)12);
-        User target1 = new User();
-        target1.setId(1L);
-        CopyOptions copyOptions = new CopyOptions();
-        copyOptions.ignoreNullValue();
-        BeanUtil.copyProperties(source1,target1, copyOptions);
-        System.out.println(JSON.toJSONString(target1));
-
+        System.out.println("---------- testDeepCopy ----------");
         testDeepCopy();
-
+        System.out.println("---------- swap ----------");
         Person xiaoZhang = new Person("小张");
         Person xiaoLi = new Person("小李");
         // Java 中只有值传递，是没有引用传递的
@@ -48,6 +28,8 @@ public class BeanCopyTest {
         swap(xiaoZhang, xiaoLi);
         System.out.println("person1:" + xiaoZhang.getName()); // 小张
         System.out.println("person2:" + xiaoLi.getName()); // 小李
+        System.out.println("---------- copyField ----------");
+        copyField();
     }
 
     public static void testDeepCopy(){
@@ -60,7 +42,7 @@ public class BeanCopyTest {
 
         UserDTO target = new UserDTO();
         BeanUtils.copyProperties(source,target);
-        System.out.println(JSON.toJSONString(target));
+        System.out.println("testDeepCopy:" + JSON.toJSONString(target));
         System.out.println(source == target); // false
         System.out.println(source.getPoem() == target.getPoem()); // true
         // 转为JSON 后重新转换
@@ -79,4 +61,23 @@ public class BeanCopyTest {
         System.out.println("swap person1:" + person1.getName()); // 小李
         System.out.println("swap person2:" + person2.getName()); // 小张
     }
+
+    public static void copyField() {
+        StudentVO studentVO = new StudentVO();
+        studentVO.setNm("zw");
+        studentVO.setId(1L);
+        studentVO.setBir(LocalDateTime.now());
+        Student student = new Student();
+        com.zw.cloud.common.utils.bean.BeanUtils.copyByCopyField(studentVO, student);
+        System.out.println("copyField:" + JSON.toJSONString(student));
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Person {
+        private String name;
+    }
+
+
 }
