369a0265b772d02d7bb9f193a646e4a25cc3821c
镜中水月 <8344832+moon-in-the-mirror@user.noreply.gitee.com>
2023-08-11 22:22:08 +0800

    feat(service): 注释掉数据库操作单元测试

--- tools/src/test/java/com/zw/cloud/tools/service/CodeServiceDemoTest.java
+++ tools/src/test/java/com/zw/cloud/tools/service/CodeServiceDemoTest.java
@@ -1,59 +1,59 @@
-package com.zw.cloud.tools.service;
-
-import com.google.common.collect.Lists;
-import com.zw.cloud.common.utils.DateTimeUtils;
-import com.zw.cloud.tools.dao.CodeDao;
-import com.zw.cloud.tools.entity.Code;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.*;
-
-import java.util.Date;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mockStatic;
-
-class CodeServiceDemoTest {
-
-    //将生成MockDao，并注入到@InjectMocks指定的类中
-    @Mock
-    CodeDao codeDao;
-
-    @InjectMocks
-    CodeServiceImpl codeService;
-
-    @BeforeEach
-     void before() {
-
-        MockitoAnnotations.openMocks(this);
-        // 解决mybatisplus orderby LambdaQueryWrapper
-        //TableInfoHelper.initTableInfo(new MapperBuilderAssistant(new MybatisConfiguration(), ""), ProductRecord.class);
-
-    }
-
-
-    @Test
-     void queryAllByLimit() {
-        Mockito.when(codeDao.queryAllByLimit(anyInt(), anyInt())).thenReturn(Lists.newArrayList());
-        Assertions.assertDoesNotThrow(() -> codeService.queryAllByLimit(1, 20));
-    }
-
-    @Test
-     void insert() {
-        Code code = new Code();
-        code.setGmtCreate(new Date());
-        code.setStatus((byte)2);
-        Mockito.doNothing().when(codeDao).insert(any());
-        Assertions.assertDoesNotThrow(() -> codeService.insert(code));
-    }
-
-    @Test
-     void plusMonths() {
-        try (MockedStatic<DateTimeUtils> dateTimeUtilsMockedStatic = mockStatic(DateTimeUtils.class)) {
-            dateTimeUtilsMockedStatic.when(()-> DateTimeUtils.plusMonths(anyInt())).thenReturn(new Date());
-            Assertions.assertDoesNotThrow(() -> DateTimeUtils.plusMonths(1));
-        }
-    }
-}
+//package com.zw.cloud.tools.service;
+//
+//import com.google.common.collect.Lists;
+//import com.zw.cloud.common.utils.DateTimeUtils;
+//import com.zw.cloud.tools.dao.CodeDao;
+//import com.zw.cloud.tools.entity.Code;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.*;
+//
+//import java.util.Date;
+//
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.ArgumentMatchers.anyInt;
+//import static org.mockito.Mockito.mockStatic;
+//
+//class CodeServiceDemoTest {
+//
+//    //将生成MockDao，并注入到@InjectMocks指定的类中
+//    @Mock
+//    CodeDao codeDao;
+//
+//    @InjectMocks
+//    CodeServiceImpl codeService;
+//
+//    @BeforeEach
+//     void before() {
+//
+//        MockitoAnnotations.openMocks(this);
+//        // 解决mybatisplus orderby LambdaQueryWrapper
+//        //TableInfoHelper.initTableInfo(new MapperBuilderAssistant(new MybatisConfiguration(), ""), ProductRecord.class);
+//
+//    }
+//
+//
+//    @Test
+//     void queryAllByLimit() {
+//        Mockito.when(codeDao.queryAllByLimit(anyInt(), anyInt())).thenReturn(Lists.newArrayList());
+//        Assertions.assertDoesNotThrow(() -> codeService.queryAllByLimit(1, 20));
+//    }
+//
+//    @Test
+//     void insert() {
+//        Code code = new Code();
+//        code.setGmtCreate(new Date());
+//        code.setStatus((byte)2);
+//        Mockito.doNothing().when(codeDao).insert(any());
+//        Assertions.assertDoesNotThrow(() -> codeService.insert(code));
+//    }
+//
+//    @Test
+//     void plusMonths() {
+//        try (MockedStatic<DateTimeUtils> dateTimeUtilsMockedStatic = mockStatic(DateTimeUtils.class)) {
+//            dateTimeUtilsMockedStatic.when(()-> DateTimeUtils.plusMonths(anyInt())).thenReturn(new Date());
+//            Assertions.assertDoesNotThrow(() -> DateTimeUtils.plusMonths(1));
+//        }
+//    }
+//}
--- tools/src/test/java/com/zw/cloud/tools/service/impl/poem/PoemServiceImplTest.java
+++ tools/src/test/java/com/zw/cloud/tools/service/impl/poem/PoemServiceImplTest.java
@@ -1,40 +1,40 @@
-package com.zw.cloud.tools.service.impl.poem;
-
-import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
-import com.google.common.collect.Lists;
-import com.zw.cloud.tools.entity.poem.Poem;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.*;
-
-
-import java.util.ArrayList;
-
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.mockStatic;
-
-class PoemServiceImplTest {
-
-    @InjectMocks
-    private PoemServiceImpl poemService;
-
-    @BeforeEach
-    void before() {
-        MockitoAnnotations.openMocks(this);
-        //TableInfoHelper.initTableInfo(new MapperBuilderAssistant(new MybatisConfiguration(), ""), Poem.class);
-
-    }
-
-    @Test
-    void testBatch(){
-        Poem poem = new Poem();
-        poem.setContent("test");
-        ArrayList<Poem> poems = Lists.newArrayList(poem);
-        try (MockedStatic<SqlHelper> sqlHelperMockedStatic = mockStatic(SqlHelper.class)) {
-            sqlHelperMockedStatic.when(() -> SqlHelper.executeBatch(any(),any(),anyCollection(),anyInt(),any())).thenReturn(true);
-            Assertions.assertDoesNotThrow(() -> poemService.saveBatch(Lists.newArrayList(poems)));
-        }
-
-    }
-}
+//package com.zw.cloud.tools.service.impl.poem;
+//
+//import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
+//import com.google.common.collect.Lists;
+//import com.zw.cloud.tools.entity.poem.Poem;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.*;
+//
+//
+//import java.util.ArrayList;
+//
+//import static org.mockito.ArgumentMatchers.*;
+//import static org.mockito.Mockito.mockStatic;
+//
+//class PoemServiceImplTest {
+//
+//    @InjectMocks
+//    private PoemServiceImpl poemService;
+//
+//    @BeforeEach
+//    void before() {
+//        MockitoAnnotations.openMocks(this);
+//        //TableInfoHelper.initTableInfo(new MapperBuilderAssistant(new MybatisConfiguration(), ""), Poem.class);
+//
+//    }
+//
+//    @Test
+//    void testBatch(){
+//        Poem poem = new Poem();
+//        poem.setContent("test");
+//        ArrayList<Poem> poems = Lists.newArrayList(poem);
+//        try (MockedStatic<SqlHelper> sqlHelperMockedStatic = mockStatic(SqlHelper.class)) {
+//            sqlHelperMockedStatic.when(() -> SqlHelper.executeBatch(any(),any(),anyCollection(),anyInt(),any())).thenReturn(true);
+//            Assertions.assertDoesNotThrow(() -> poemService.saveBatch(Lists.newArrayList(poems)));
+//        }
+//
+//    }
+//}
